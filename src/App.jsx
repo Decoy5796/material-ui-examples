4d8b116863dc07b51493a4c0d155e1ba3618ae0e
Decoy5796 <kkw5796@gmail.com>
2020-12-26 23:00:07 +0900

    Material-UI 특징 알아보기 - 기본 컴포넌트 사용해보기 - 테마 커스터마징 방법 - 스타일 오버라이딩 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Button, Typography } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => createStyles({
+  helloThereStyle: {
+    fontStyle: 'oblique',
+    color: 'red',
+    fontSize: '30px'
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div>
-      App
+      <Typography className={classes.helloThereStyle} variant='h1' color="primary">Hello There</Typography>
+      <Button color='secondary' variant='outlined'>
+        This is our first button
+      </Button>
     </div>
   );
 }
