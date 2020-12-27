f4995271d81e1e97a9412d1322dc51538c9ced11
Decoy5796 <kkw5796@gmail.com>
2020-12-27 20:30:44 +0900

    테마를 사용하여 모드 전환하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Grid,
+  Typography,
+  Button,
+  Paper,
+  Switch,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core';
+import { lightBlue } from '@material-ui/core/colors';
 
 function App() {
-  return <div>App</div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  // const theme = createMuiTheme({
+  //   palette: {
+  //     type: darkMode ? 'dark' : 'light',
+  //   },
+  // });
+
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  const lightTheme = createMuiTheme({
+    palette: {
+      primary: lightBlue,
+      secondary: lightBlue,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Paper style={{ height: '100vh' }}>
+        <Grid container direction='column'>
+          <Typography variant='h1'>This is My App</Typography>
+          <Button variant='contained' color='primary'>
+            This is a button
+          </Button>
+          <Button variant='contained' color='secondary'>
+            This is another button
+          </Button>
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Grid>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
